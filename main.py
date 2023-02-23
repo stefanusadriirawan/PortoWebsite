bd5ad8ae01d2930cefeb269772934478a61d4bf1
Stefanus Adri Irawan <117265543+stefanusadriirawan@users.noreply.github.com>
2023-02-24 05:40:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,165 +1,105 @@
-# Importing required libraries
 import streamlit as st
 
+# options=["Home", "About", "Skills", "Projects", "Contact"],
+# icons=["house", "person-circle", "code-slash", "person-workspace", "telephone"],
+
+# navbar home -----------------------------------------------------------------------------
+def home():
+    # Defining the layout
+    col1, col2 = st.columns(2)
+    with col1:
+        st.image("Potrait_photo.png", use_column_width=True)
+    with col2:
+        st.title("Stefanus Adri Irawan")
+        st.subheader("Data Analyst")
+        st.write("""
+        I am an experienced web developer with expertise in Python, Flask, Django, and React. 
+        I have worked on several projects for clients from various industries, including healthcare, finance, and e-commerce. 
+        In my free time, I like to contribute to open-source projects and learn new technologies. 
+        """)
+    st.write("---")
 
 
-#from streamlit_option_menu import option_menu
+# navbar about -----------------------------------------------------------------------------
+def about():
+    st.title("About Page")
+    st.write("This is my portfolio website. I'm a Python developer and love creating cool things with Streamlit!")
 
-
-
-
-# Setting the page title
-st.set_page_config(page_title="My Portfolio Website")
-
-#options=["Home", "About", "Skills", "Projects", "Contact"],
-        #icons=["house", "person-circle", "code-slash", "person-workspace", "telephone"],
-
-# navbar -----------------------------------------------------------------------------
-
-# as sidebar menu
-st.markdown(
-    """
-    <style>
-    
-    [title~="st.iframe"] { width: 100%}
-    
-    /* For mobile phones: */
-    [class*="col-"] {
-        width: 100%;
-    }
-    
-    /* For tablets and up: */
-    @media only screen and (min-width: 375x) {
-        /* Three equal columns */
-        .col-3 {
-            float: left;
-            width: 33.33%;
-        }
-    }
-        
-    body {
-        margin: 0;
-        padding: 0;
-        font-family: sans-serif;
-    }
-    
-    header {
-        background-color: #f2f2f2;
-        padding: 10px;
-        box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-    }
-    
-    nav {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        width: 100%;
-    }
-    
-    .logo a {
-        font-size: 20px;
-        font-weight: bold;
-        color: #333;
-        text-decoration: none;
-    }
-    
-    ul {
-        display: flex;
-        justify-content: center; /* center horizontally */
-        align-items: center; /* center vertically */
-        list-style: none;
-        margin: 0;
-        padding: 0;
-    }
-    
-    li {
-        margin: 0 10px;
-    }
-    
-    a {
-        color: #333;
-        text-decoration: none;
-    }
-    
-    a:hover {
-        text-decoration: underline;
-    }
-    
-    /* Media queries for responsive design */
-    @media only screen and (max-width: 375px) {
-        .logo a {
-            font-size: 16px;
-        }
-        li {
-            margin: 0 5px;
-        }
-    }
-    </style>
-    
-
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Minimalist Navbar</title>
-        <link rel="stylesheet" type="text/css" href="navbar.css">
-    </head>
-    <body>
-        <header>
-            <nav>
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
-        </header>
-        <br>
-        
-    </body>
-    </html>
-    
-""", unsafe_allow_html=True)
-
-
-# isi page -----------------------------------------------------------------------------
-
-# Defining the layout
-col1, col2 = st.columns(2)
-with col1:
-    st.image("Potrait_photo.png", use_column_width=True)
-with col2:
-    st.title("Stefanus Adri Irawan")
-    st.subheader("Data Analyst")
-    st.write("""
-    I am an experienced web developer with expertise in Python, Flask, Django, and React. 
-    I have worked on several projects for clients from various industries, including healthcare, finance, and e-commerce. 
-    In my free time, I like to contribute to open-source projects and learn new technologies. 
-    """)
-st.write("---")
+# navbar skills -----------------------------------------------------------------------------
+def skills():
 # Adding a section for skills
-st.header("Skills")
-st.write("""
-- Python
-- Flask
-- Django
-- React
-- HTML/CSS
-""")
+    st.header("Skills")
+    st.write("""
+    - Python
+    - Flask
+    - Django
+    - React
+    - HTML/CSS
+    """)
+# navbar projects -----------------------------------------------------------------------------
+def projects():
+  # Adding a section for projects
+    st.header("Projects")
+    st.write("""
+    - [Project 1](https://github.com/project1)
+    - [Project 2](https://github.com/project2)
+    - [Project 3](https://github.com/project3)
+    """)
 
-# Adding a section for projects
-st.header("Projects")
-st.write("""
-- [Project 1](https://github.com/project1)
-- [Project 2](https://github.com/project2)
-- [Project 3](https://github.com/project3)
-""")
+# navbar contact -----------------------------------------------------------------------------
+def contact():
+   # Adding a section for contact information
+    st.header("Contact")
+    st.write("""
+    - Email: john.doe@email.com
+    - Phone: +1-123-456-7890
+    - LinkedIn: 
+    """)
 
-# Adding a section for contact information
-st.header("Contact")
-st.write("""
-- Email: john.doe@email.com
-- Phone: +1-123-456-7890
-- LinkedIn: 
-""")
+# lem pemersatu -----------------------------------------------------------------------------
+
+with st.container():
+    col1, col2, col3, col4, col5 = st.columns(5)
+    with col1:
+        home_button = st.button("Home", key="home",)
+    with col2:
+        about_button = st.button("About", key="about", )
+    with col3:
+        skills_button = st.button("Skills", key="skills", )
+    with col4:
+        projects_button = st.button("Projects", key="projects", )
+    with col5:
+        contact_button = st.button("Contact", key="contact", )
+
+# Get the current query parameters
+params = st.experimental_get_query_params()
+
+
+# Check if the page parameter is not set or set to "home"
+if "page" not in params:
+    home_button = True
+else:
+    home_button = False
+
+if home_button:
+    st.experimental_set_query_params(page="home")
+    home()
+elif about_button:
+    st.experimental_set_query_params(page="about")
+    about()
+elif skills_button:
+    st.experimental_set_query_params(page="skills")
+    skills()
+elif projects_button:
+    st.experimental_set_query_params(page="projects")
+    projects()
+elif contact_button:
+    st.experimental_set_query_params(page="contact")
+    contact()
+else:
+    st.experimental_set_query_params(page="home")
+    home()
+
+
+
+
